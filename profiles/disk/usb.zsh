374d3b50e35d829e3f60e7dfd276ada9432e83fe
tokiclover <tokiclover@gmail.com>
2014-10-15 18:10:10 +0200

    profiles/disk/usb.zsh: do not apply noop blindly

--- profiles/disk/usb.zsh
+++ profiles/disk/usb.zsh
@@ -3,7 +3,10 @@
 #
 
 for dev (/sys/devices/pci*/*/usb*/*/*/host*/target*/*/block/*sd[a-z](/N))
-	echo noop  >$dev/queue/scheduler
+	case "$(< $dev/removable)" in
+		(1) echo noop >$dev/queue/scheduler;;
+		(0) echo cfq  >$dev/queue/scheduler;;
+	esac
 
 #
 # vim:fenc=utf-8:ft=zsh:ci:pi:sts=0:sw=4:ts=4:
